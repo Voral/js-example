1f01944aa569780e3b06258327c2271e4cfde2fc
Alexander Vorobyev <voral.adm@gmail.com>
2025-01-22 23:18:02 +0300

    dev: Динамическое добавление кнопок

--- toolbar.js
+++ toolbar.js
@@ -1,10 +1,34 @@
+const REMOVE_ME_EVENT = 'exampleRemoveMe';
+
 class Toolbar {
     constructor(options) {
         this.element = options.element || null;
         this.buttons = options.buttons || [];
+        this.demoButtons = [];
+        this.createPanels();
         this.initButtons();
         this.initSelfButtons();
         this.customButtonsDisabled = false;
+        this.element.addEventListener(REMOVE_ME_EVENT, this.onRemoveQuery.bind(this));
+    }
+
+    onRemoveQuery(e) {
+        for (const button of this.demoButtons) {
+            if (button.button === e.target) {
+                this.removeButton(button)
+                this.demoButtons.splice(this.demoButtons.indexOf(button), 1);
+                break;
+            }
+        }
+    }
+
+    createPanels() {
+        this.editorPanel = document.createElement('div');
+        this.editorPanel.classList.add('toolbar__panel');
+        this.demoPanel = document.createElement('div');
+        this.demoPanel.classList.add('toolbar__panel');
+        this.element.appendChild(this.editorPanel);
+        this.element.appendChild(this.demoPanel);
     }
 
     initSelfButtons() {
@@ -13,13 +37,13 @@ class Toolbar {
             name: 'Disable',
             handler: this.disableOther
         })
-        this.element.appendChild(this.btnDisable.getButton());
+        this.demoPanel.appendChild(this.btnDisable.getButton());
         this.btnDelete = new Button({
             managed: false,
             name: 'Delete',
             handler: this.deleteOther
         })
-        this.element.appendChild(this.btnDelete.getButton());
+        this.demoPanel.appendChild(this.btnDelete.getButton());
     }
 
     deleteOther = () => {
@@ -38,6 +62,7 @@ class Toolbar {
             button.getButton().remove();
         }
     }
+
     disableOther = (e) => {
         this.customButtonsDisabled = !this.customButtonsDisabled;
         e.target.innerText = this.customButtonsDisabled ? 'Enable' : 'Disable';
@@ -50,8 +75,29 @@ class Toolbar {
         for (const button of this.buttons) {
             button.managed = true;
             button.instance = new Button(button);
-            this.element.appendChild(button.instance.getButton());
+            this.editorPanel.appendChild(button.instance.getButton());
         }
+    }
+
+    addButton(title, handler) {
+        const options = {
+            name: title,
+            handler: handler,
+            managed: true
+        }
+        options.instance = new Button(options);
+        this.editorPanel.appendChild(options.instance.getButton());
+    }
+
+    addDemoButton(title, handler) {
+        const options = {
+            name: title,
+            handler: handler,
+            managed: true
+        }
+        options.instance = new Button(options);
+        this.demoPanel.insertBefore(options.instance.getButton(), this.demoPanel.firstChild);
+        this.demoButtons.push(options.instance);
     }
 
     destroy() {
